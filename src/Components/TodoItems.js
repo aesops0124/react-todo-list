968c74b273a178d7d04a1a32f61c0627b5988b36
Tommy Hui <aesops0124@gmail.com>
2018-12-03 23:48:27 +0800

    feat: display the to-do items

--- src/Components/TodoItems.js
+++ src/Components/TodoItems.js
@@ -1,15 +1,20 @@
-import React, { Component } from 'react'
-
+import React, { Component } from "react";
+ 
 class TodoItems extends Component {
   createTasks(item) {
     return <li key={item.key}>{item.text}</li>
   }
+ 
   render() {
-    const todoEntries = this.props.entries
-    const listItems = todoEntries.map(this.createTasks)
-
-    return <ul className="theList">{listItems}</ul>
+    var todoEntries = this.props.entries;
+    var listItems = todoEntries.map(this.createTasks);
+ 
+    return (
+      <ul className="theList">
+          {listItems}
+      </ul>
+    );
   }
-}
-
-export default TodoItems
+};
+ 
+export default TodoItems;
